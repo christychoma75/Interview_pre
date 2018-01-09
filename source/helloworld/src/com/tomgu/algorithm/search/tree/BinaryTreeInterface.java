e99b0fb18b676600ec72aee2b6b5a0b7b57a9f3b
tomgu1991 <guzuxing1991@gmail.com>
2018-01-09 17:26:17 +0800

    remove unused interface

--- source/helloworld/src/com/tomgu/algorithm/search/tree/BinaryTreeInterface.java
+++ source/helloworld/src/com/tomgu/algorithm/search/tree/BinaryTreeInterface.java
@@ -4,7 +4,7 @@ package com.tomgu.algorithm.search.tree;
  * Created by guzuxing on 08/01/2018.
  * binary tree interface
  */
-public interface BinaryTreeInterface<T> extends TreeInterface<T>, TreeIteratorInterface {
+public interface BinaryTreeInterface<T> extends TreeInterface<T> {
   public void setTree(T rootData);
   public void setTree(T rootData, BinaryTreeInterface<T> leftTree, BinaryTreeInterface<T> rightTree);
 }
